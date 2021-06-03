c193a0eddabfa2aab3aead594fd8d8c4ffc02e78
Parth <parthprime143@gmail.com>
2021-06-03 22:36:01 +0530

    First main menu commit project...>>>

---
+++ Boxy/Assets/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    string Play;
+    
+    public void PlayGame()
+    {
+      
+    }
+}
